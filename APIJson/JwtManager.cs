42a1518a56d450357c1f86a1f99b9d0826377fc9
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-07-25 11:53:29 -0300

    Duende.IdentityServer

---
+++ APIJson/JwtManager.cs
@@ -0,0 +1,66 @@
+﻿using Duende.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace APIJson
+{
+    public class JwtManager
+    {
+
+        private async Task<Tuple<string, string>> GenerateToken(IdentityUser user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_jwtSettings.Key));
+
+            var userClaims = await _userManager.GetClaimsAsync(user);
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var roleClaims = new List<Claim>();
+
+            foreach (var item in roles)
+            {
+                roleClaims.Add(new Claim(ClaimTypes.Role, item));
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                    {
+                        new Claim("Id",user.Id),
+                        new Claim(JwtRegisteredClaimNames.Email,user.Email),
+                        new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                        new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                    }.Union(userClaims)
+                     .Union(roleClaims)
+                    ),
+                Expires = DateTime.UtcNow.Add(_jwtSettings.ExpireTime),
+                SigningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = jwtTokenHandler.WriteToken(token);
+
+            var refreshToken = new RefreshToken
+            {
+                JwtId = token.Id,
+                IsUsed = false,
+                IsRevoked = false,
+                UserId = user.Id,
+                CreatedDate = DateTime.UtcNow,
+                ExpireDate = DateTime.UtcNow.AddMonths(6),
+                Token = $"{GenerateRandomTokenCharacters(35)}{Guid.NewGuid()}"
+            };
+
+            await _cleanArchitectureIdentityDbContext.RefreshTokens!.AddAsync(refreshToken);
+            await _cleanArchitectureIdentityDbContext.SaveChangesAsync();
+
+            return new Tuple<string, string>(jwtToken, refreshToken.Token);
+
+
+        }
+    }
+}
